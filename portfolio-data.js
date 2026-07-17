377c3e7253897bdef612bf2fcbbb175bed8c9ec6
TriBeCreatives <sixthstreet006@gmail.com>
2026-07-18 00:06:16 +0800

    Update portfolio-data.js

---
+++ portfolio-data.js
@@ -0,0 +1,14 @@
+const PORTFOLIO_DATA = [
+  {
+    src: 'assets/Portfolio/Birthday-Decorations.jpg',
+    eventType: 'Birthday',
+    title: 'Birthday Decorations',
+    bg: '#1d1520'
+  },
+  {
+    src: 'assets/Portfolio/Wedding.jpg',
+    eventType: 'Wedding',
+    title: 'Wedding Setup',
+    bg: '#1d1520'
+  }
+];
